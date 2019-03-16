e5efcbd27036fa6874d497e7c10f0b6c7a775b8f
Benjamin de Castro Azevedo P <36571620+Heroadn@users.noreply.github.com>
2019-03-16 14:01:32 -0300

    Add files via upload

--- questionario/template/default/template.php
+++ questionario/template/default/template.php
@@ -3,23 +3,112 @@
 		public $_title = '1aasda';
 
 		 public static function getHeaders($title) {
+			//
+			var path = '/questionario';
+			 
 			echo "<head>\n";
 			echo '		<title>' . $title . "</title>\n";
-			echo '		<link href="' .  '/questionario/core/frontend/css/core.css" rel="stylesheet">' . "\n";
-			echo '		<link href="' .  '/questionario/core/frontend/css/bootstrap.min.css" rel="stylesheet">' . "\n";
-			echo '		<link href="' .  '/questionario/core/frontend/js/font-awesome/css/font-awesome.min.css" rel="stylesheet">' . "\n";
-			echo '		<script src="' . '/questionario/core/frontend/js/jquery.js"></script>' . "\n";
-			echo '		<script src="' . '/questionario/core/frontend/js/bootstrap.min.js"></script>' . "\n";
-			echo '		<script src="' . '/questionario/core/frontend/js/canvasjs.min.js"></script>' . "\n";
+			
+			echo '<meta charset="utf-8">';
+			echo '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">';
+			echo '		<link href="' . $path . '/core/frontend/css/core.css" rel="stylesheet">' . "\n";
+			echo '		<link href="' . $path .  '/core/frontend/css/bootstrap.min.css" rel="stylesheet">' . "\n";
+			echo '		<link href="' . $path .  '/core/frontend/js/font-awesome/css/font-awesome.min.css" rel="stylesheet">' . "\n";
+			echo '		<script src="' . $path . '/core/frontend/js/jquery.js"></script>' . "\n";
+			echo '		<script src="' . $path . '/core/frontend/js/bootstrap.min.js"></script>' . "\n";
+			echo '		<script src="' . $path . '/core/frontend/js/canvasjs.min.js"></script>' . "\n";
 			echo '	</head>';
+			
+			require '../../menu.php';
 			
 			echo '<style>
 					body {
-						background-color: #25272F;
-						background-image: url("/questionario/template/default/images/algorithm.jpg");
-						background-repeat: no-repeat;
-						background-size: cover;
+							background-color: #25272F;
+							background-image: url("'. $path .'/template/default/images/algorithm.jpg");
+							background-repeat: no-repeat;
+							background-size: cover;
+						}
+			
+					/* Extra small devices (phones, up to 480px) */
+					@media screen and (max-width: 767px) {
+						.panel-default>.panel-heading{
+							font-size: 4.0vw;
+						}
+						
+						.panel-info>.panel-heading {
+							font-size: 4.0vw;
+						}
+						
+						.panel-body {
+							font-size: 4.0vw;
+						}
+						
+						input[type=checkbox], input[type=radio] {
+							width:30px;
+							height:30px;
+						}
 					}
+					
+					/* Small devices (tablets, 768px and up) */
+					@media (min-width: 768px) and (max-width: 991px) {
+						.panel-default>.panel-heading{
+							font-size: 4.0vw;
+						}
+						
+						.panel-info>.panel-heading {
+							font-size: 4.0vw;
+						}
+						
+						.panel-body {
+							font-size: 4.0vw;
+						}
+						
+						input[type=checkbox], input[type=radio] {
+							width:30px;
+							height:30px;
+						}
+					}
+					
+					/* tablets/desktops and up ----------- */
+					@media (min-width: 992px) and (max-width: 1199px) {	
+						.panel-default>.panel-heading{
+							font-size: 2.0vw;
+						}
+						
+						.panel-info>.panel-heading {
+							font-size: 1.5vw;
+						}
+						
+						.panel-body {
+							font-size: 1.5vw;
+						}
+						
+						input[type=checkbox], input[type=radio] {
+							width:20px;
+							height:20px;
+						}
+					}
+					
+					/* large desktops and up ----------- */
+					@media screen and (min-width: 1200px) {	
+						.panel-default>.panel-heading{
+							font-size: 2.0vw;
+						}
+						
+						.panel-info>.panel-heading {
+							font-size: 1.5vw;
+						}
+						
+						.panel-body {
+							font-size: 1.5vw;
+						}
+						
+						input[type=checkbox], input[type=radio] {
+							width:20px;
+							height:20px;
+						}
+					}
+
 					.starter-template {
 						padding: 0px 15px;
 					}
